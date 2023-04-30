1e38888a014291f3b815e313d1e9f16456bfec6c
THE2TRELOK <98739146+THE2TRELOK@users.noreply.github.com>
2023-04-30 03:12:39 +0300

    data

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import jsonData from './r.json';
 import './App.css';
 
 function App() {
-    const [data, setData] = useState(jsonData.range);
+    const [data, data2] = useState(jsonData.range);
 
     const renderItem = React.useCallback((rowIndex) => {
         return (
             <div
                 className="list-item"
                 style={{
-                    height: 40,
+                    height: 50,
                     background: rowIndex % 2 ? "lightgray" : "white",
                     display: "flex",
                     alignItems: "center"
                 }}
             >
-                <h3>{data[rowIndex][0]}</h3>
+                <h3>{data[rowIndex][0]}</h3> 
+                <h3>{data[rowIndex][1]}</h3>
+                <h3>{data[rowIndex][2]}</h3>
+                <h3>{data[rowIndex][3]}</h3>
+                <h3>{data[rowIndex][4]}</h3>
+                
             </div>
         );
     }, []);
