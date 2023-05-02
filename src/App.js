b7975b5a88919b1effeedbdb64742cc003aca3b6
THESTRELOK <ggamersshd@gmail.com>
2023-05-02 21:39:06 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,42 +5,49 @@ import jsonData from './r.json';
 import './App.css';
 
 function App() {
-    const [data, data2] = useState(jsonData.range);
+  const [data, setData] = useState(jsonData.range);
 
-    const renderItem = React.useCallback((rowIndex) => {
-        return (
-            <div
-                className="list-item"
-                style={{
-                    height: 50,
-                    background: rowIndex % 2 ? "lightgray" : "white",
-                    display: "flex",
-                    alignItems: "center"
-                }}
-            >
-                <h3>{data[rowIndex][0]}</h3> 
-                <h3> | {data[rowIndex][1]} | </h3>
-                <h3> {data[rowIndex][2]} | </h3>
-                <h3>{data[rowIndex][3]}</h3>
-                <h3> | {data[rowIndex][4]} |</h3>
-                
-            </div>
-        );
-    }, []);
+  const renderItem = React.useCallback((rowIndex) => {
+    const firstLetter = data[rowIndex][0][0];
+    const containsX = data[rowIndex][0][0].includes('x');
+
+    const style = {
+      height: 50,
+      border: 2,
+      background: rowIndex % 2 ? "lightgray" : "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    };
+
+    const letterStyle = {
+      background: containsX && firstLetter !== 'x' ? "red" : "transparent",
+    };
 
     return (
-        <div className={"data-container"}>
-            {data.length > 0 &&
-                <VirtualScroll
-                    className="list"
-                    minItemHeight={40}
-                    totalLength={data.length}
-                    length={40}
-                    renderItem={renderItem}
-                />
-            }
-        </div>
+      <div className="list-item" style={style}>
+        <h3 style={letterStyle}>{firstLetter}</h3>
+        <h3>{data[rowIndex][0][1]}</h3>
+        <h3>{data[rowIndex][0][2]}</h3>
+        <h3>{data[rowIndex][0][3]}</h3>
+        <h3>{data[rowIndex][0][4]}</h3>
+      </div>
     );
+  }, [data]);
+
+  return (
+    <div className={"data-container"}>
+      {data.length > 0 && (
+        <VirtualScroll
+          className="list"
+          minItemHeight={40}
+          totalLength={data.length}
+          length={40}
+          renderItem={renderItem}
+        />
+      )}
+    </div>
+  );
 }
  
 export default App;
