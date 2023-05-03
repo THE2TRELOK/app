f5829790d3428c3dd28d56372f4020177acb49fe
THESTRELOK <ggamersshd@gmail.com>
2023-05-03 10:33:43 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
   const [data, setData] = useState(jsonData.range);
 
   const renderItem = React.useCallback((rowIndex) => {
-    const firstLetter = data[rowIndex][0][0];
-    const containsX = data[rowIndex][0][0].includes('x');
-
     const style = {
       height: 50,
       border: 2,
@@ -20,26 +17,41 @@ function App() {
       justifyContent: "center",
     };
 
-    const letterStyle = {
-      background: containsX && firstLetter !== 'x' ? "red" : "transparent",
-    };
+    const elements = [];
 
-    return (
-      <div className="list-item" style={style}>
-        <h3 style={letterStyle}>{firstLetter}</h3>
-        <h3>{data[rowIndex][0][1]}</h3>
-        <h3>{data[rowIndex][0][2]}</h3>
-        <h3>{data[rowIndex][0][3]}</h3>
-        <h3>{data[rowIndex][0][4]}</h3>
-      </div>
-    );
+    for (let i = 1; i <= 4; i++) {
+      const text = data[rowIndex][0][i];
+      let backgroundColor = "transparent";
+
+      if (text.includes("x")) {
+        backgroundColor = "red";
+      } else if (text.includes("y")) {
+        backgroundColor = "green";
+      } else if (text.includes("z")) {
+        backgroundColor = "lightblue";
+      } else if (text.includes("w")) {
+        backgroundColor = "yellow";
+      }
+
+      const letterStyle = {
+        background: backgroundColor,
+      };
+
+      elements.push(
+        <h3 key={i} style={letterStyle}>
+          {text}
+        </h3>
+      );
+    }
+
+    return <div className="list-item" style={style}>{elements}</div>;
   }, [data]);
 
   return (
     <div className={"data-container"}>
       {data.length > 0 && (
         <VirtualScroll
-          className="list"
+          className={"list"}
           minItemHeight={40}
           totalLength={data.length}
           length={40}
