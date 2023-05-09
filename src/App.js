e77c4c96547e72a4071ecde992c14fa4369b26b7
THESTRELOK <ggamersshd@gmail.com>
2023-05-10 01:51:24 +0400

    AA

--- src/App.js
+++ src/App.js
@@ -5,7 +5,33 @@ import jsonData from './r.json';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(jsonData.range);
+  const [filterA, setFilterA] = useState('');
+  const [_data, _setData] = useState(jsonData.range);
+
+  const methodFilterA = ( data, count ) => {
+    let str = "";
+
+    if( count == 0 )
+      return true;
+      // ['A', 'A', 'Q', '7', '5'] -> AAQ75
+      data[0].map( i => { str += i[0] });
+//check
+    if( str.match(/\A/g) && str.match(/\A/g).length == count ) 
+      return true;
+
+    return false
+  }
+
+  const handleInputChange = (event) => {
+    let val = event.target.value;
+    
+    val = val.replace("a", "A");
+    let result = val.replace(/[^A]/g, "");
+
+    setFilterA(result);
+
+    _setData(jsonData.range.filter( i => methodFilterA(i, result.length) ))
+  };
 
   const renderItem = React.useCallback((rowIndex) => {
     const style = {
@@ -17,44 +43,29 @@ function App() {
       justifyContent: "center",
     };
 
-    //OPTION 1: old but working
-
-    // const elements = [];
-    // for (let i = 0; i < data[rowIndex][0].length; i++) {
-    //   const card = data[rowIndex][0][i];
-    //
-    //   const rank = card[0];
-    //   const suit = card[1];
-    //
-    //   elements.push(
-    //     <h3 key={i} className={`card ${suit}`}>
-    //       {rank}
-    //     </h3>
-    //   );
-    // }
-
-
-    //OPTION 2: modern with array.map usage
-    const elements = data[rowIndex][0].map((card, index) => (
+    const elements = _data[rowIndex][0].map((card, index) => (
       <h3 key={index} className={`card ${card[1]}`}>
         {card[0]}
       </h3>
     ))
 
     return <div className="list-item" style={style}>{elements}</div>;
-  }, [data]);
+  }, [_data]);
 
   return (
-    <div className={"data-container"}>
-      {data.length > 0 && (
-        <VirtualScroll
-          className={"list"}
-          minItemHeight={40}
-          totalLength={data.length}
-          length={40}
-          renderItem={renderItem}
-        />
-      )}
+    <div>
+      <div className={"data-container"}>
+        {_data.length > 0 && (
+          <VirtualScroll
+            className={"list"}
+            minItemHeight={40}
+            totalLength={_data.length}
+            length={40}
+            renderItem={renderItem}
+          />
+        )}
+      </div>
+      <input type='text' value={filterA} maxLength={4} minLength={0} placeholder='Type number' onChange={handleInputChange}/>
     </div>
   );
 }
